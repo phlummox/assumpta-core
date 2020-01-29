0149ba0828caffcc086d0607ad7b34ce98be5164
phlummox <phlummox@localhost>
2020-01-29 03:28:42 +0000

    add convenience script for running doctests with stack

---
+++ scripts/stack-test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# run 'stack-only' tests with some resolver
+
+if [ "$#" -ne 1 ]; then
+  echo "expected one arg, a resolver to use (e.g. 'lts-11')"
+  exit 1
+fi
+
+export STACK_RESOLVER="$1"
+stack --resolver="${STACK_RESOLVER}" test --flag assumpta-core:test-doctests
+
